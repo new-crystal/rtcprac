77f3e21af56a7558d3b8539abd09d1b1bdeb74d9
new-crystal <oj3339@naver.com>
2023-02-02 08:50:50 +0900

    add firebase

--- webRTC/rtcprac/src/Stream.jsx
+++ webRTC/rtcprac/src/Stream.jsx
@@ -1,10 +1,21 @@
 import { useEffect, useState } from "react";
+import { addDoc, collection, getDocs } from "firebase/firestore";
 import { useRef } from "react";
+import { db } from "./server/firebase";
+import { async, uuidv4 } from "@firebase/util";
 
 const Stream = () => {
   const video_ref = useRef();
   const [audio, setAudio] = useState(false);
   const [video, setVideo] = useState(true);
+  const join = false;
+
+  const data = {
+    roomId: uuidv4(),
+    userId: uuidv4(),
+    audio,
+    video,
+  };
 
   const getUserVideo = async () => {
     if (video === true) {
@@ -35,12 +46,25 @@ const Stream = () => {
     getUserVideo();
   }, [audio, video]);
 
-  const onClickSoundBtn = () => {
+  const onClickSoundBtn = async () => {
     setAudio(!audio);
+    await addDoc(collection(db, "meetting-test"), data);
   };
 
-  const onClickVideoBtn = () => {
+  const onClickVideoBtn = async () => {
     setVideo(!video);
+    await addDoc(collection(db, "meetting-test"), data);
+  };
+
+  const onClickJoinBtn = async () => {
+    //useParams로 받아온 아이디
+    let id = null;
+    const dataList = await getDocs(collection(db, "meetting"));
+    dataList.forEach((data) => {
+      data.roomId === id ? (join = true) : (join = false);
+    });
+    //
+    //join ? navigate(`/meet/${id}`) : alert("입장하려는 방이 없습니다.")
   };
 
   return (
@@ -48,6 +72,7 @@ const Stream = () => {
       <video controls ref={video_ref}></video>
       <button onClick={onClickSoundBtn}>sound</button>
       <button onClick={onClickVideoBtn}>camera</button>
+      <button onClick={onClickJoinBtn}>join</button>
     </div>
   );
 };
---
+++ webRTC/rtcprac/src/server/firebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getDatabase } from "firebase/database";
+import { GoogleAuthProvider } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const database = getDatabase(app);
+export const provider = new GoogleAuthProvider();
