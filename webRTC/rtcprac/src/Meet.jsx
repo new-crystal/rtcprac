0d016b6c3fdd21757d9609cffdcbce7af23fe606
new-crystal <oj3339@naver.com>
2023-02-06 08:47:09 +0900

    add meet

--- webRTC/rtcprac/src/Meet.jsx
+++ webRTC/rtcprac/src/Meet.jsx
@@ -3,12 +3,13 @@ import { useEffect } from "react";
 import { useRef } from "react";
 import { servers } from "./server/firebase";
 import { db } from "./server/firebase";
-import { addDoc, collection, getDocs } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
 import { useState } from "react";
 
 const Meet = () => {
   const myVideo_ref = useRef();
   const yourVideo_ref = useRef();
+  const call_ref = useRef();
   const [audio, setAudio] = useState(true);
   const [video, setVideo] = useState(true);
 
@@ -52,14 +53,99 @@ const Meet = () => {
     setVideo(data.video);
   }
 
+  async function onClickEndBtn() {
+    // referencing firebase collections
+    const callDoc = db.collection("calls").doc();
+    const offerCandidates = callDoc.collection("offerCandidates");
+    const answerCandidiates = callDoc.collection("answerCandidates");
+    // setting the input value to the calldoc id
+    call_ref.current.value = callDoc.id;
+    // get candidiates for caller and save to db
+    pc.onicecandidate = (event) => {
+      event.candidate && offerCandidates.add(event.candidate.toJSON());
+    };
+    // create offer
+    const offerDescription = await pc.createOffer();
+    await pc.setLocalDescription(offerDescription);
+    // config for offer
+    const offer = {
+      sdp: offerDescription.sdp,
+      type: offerDescription.type,
+    };
+    await callDoc.set({ offer });
+    // listening to changes in firestore and update the streams accordingly
+    callDoc.onSnapshot((snapshot) => {
+      const data = snapshot.data();
+      if (!pc.currentRemoteDescription && data.answer) {
+        const answerDescription = new RTCSessionDescription(data.answer);
+        pc.setRemoteDescription(answerDescription);
+      }
+      // if answered add candidates to peer connection
+      answerCandidiates.onSnapshot((snapshot) => {
+        snapshot.docChanges().forEach((change) => {
+          if (change.type === "added") {
+            const candidate = new RTCIceCandidate(change.doc.data());
+            pc.addIceCandidate(candidate);
+          }
+        });
+      });
+    });
+    // hangupButton.disabled = false;
+  }
+
+  const addPeople = async () => {
+    //const callId = callInput.value;
+    const callId = "dddd";
+    // getting the data for this particular call
+    const callDoc = db.collection("meettingtest").doc(callId);
+
+    const answerCandidates = callDoc.collection("answerCandidates");
+    const offerCandidates = callDoc.collection("offerCandidates");
+
+    // here we listen to the changes and add it to the answerCandidates
+    pc.onicecandidate = (event) => {
+      event.candidate && answerCandidates.add(event.candidate.toJSON());
+    };
+
+    const callData = (await callDoc.get()).data();
+
+    // setting the remote video with offerDescription
+    const offerDescription = callData.offer;
+    await pc.setRemoteDescription(new RTCSessionDescription(offerDescription));
+
+    // setting the local video as the answer
+    const answerDescription = await pc.createAnswer();
+    await pc.setLocalDescription(new RTCSessionDescription(answerDescription));
+
+    // answer config
+    const answer = {
+      type: answerDescription.type,
+      sdp: answerDescription.sdp,
+    };
+
+    await callDoc.update({ answer });
+
+    offerCandidates.onSnapshot((snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+        if (change.type === "added") {
+          let data = change.doc.data();
+          pc.addIceCandidate(new RTCIceCandidate(data));
+        }
+      });
+    });
+  };
+
   useEffect(() => {
     getVideo();
+    addPeople();
   }, []);
 
   return (
     <div>
-      <video ref={myVideo_ref}></video>
-      <video ref={yourVideo_ref}></video>
+      <input ref={call_ref} />
+      <video autoPlay ref={myVideo_ref}></video>
+      <video autoPlay ref={yourVideo_ref}></video>
+      <button onClick={onClickEndBtn}>종료</button>
     </div>
   );
 };
