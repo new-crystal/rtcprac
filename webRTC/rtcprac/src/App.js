9396a3944e5c385973a6a5ce01005618433fb881
new-crystal <oj3339@naver.com>
2023-02-11 15:49:48 +0900

    edit meet

--- webRTC/rtcprac/src/App.js
+++ webRTC/rtcprac/src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
-import Meet from "./Meet";
+import Meet from "./Meet_old";
 import Stream from "./Stream";
+import { database } from "./server/firebase";
+import { push, ref, set } from "firebase/database";
+import { useEffect } from "react";
 
 function App() {
   return (
